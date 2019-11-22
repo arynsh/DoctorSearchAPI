d063709ff8f795514cacad950fa9ea12411ac093
Sharon Lee <sharonlee714@gmail.com>
2019-11-22 08:39:36 -0800

    add webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 
 module.exports = {
@@ -17,9 +18,10 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
+    new Dotenv(),
     new UglifyJsPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Ping Pong',
+      title: 'DoctorsAPI',
       template: './src/index.html',
       inject: 'body'
     })
